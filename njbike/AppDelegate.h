40f59e92d0ab48bf05cd072f46e32c84651ef3cd
Yueping.Sun <yueping.sun.gm@gmail.com>
2013-04-22 10:08:23 +0800

    Initial Commit

---
+++ njbike/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  njbike
+//
+//  Created by suny on 13-4-22.
+//  Copyright (c) 2013年 suny. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ njbike/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  njbike
+//
+//  Created by suny on 13-4-22.
+//  Copyright (c) 2013年 suny. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
